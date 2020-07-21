d05bb36649a4c5b60c151bc9ae50790fe46d8582
bryanchoi <bryanxchoi@gmail.com>
2020-07-21 14:56:24 -0700

    Fix Transaction Exception Bug
    Transactions that were explicitly closed would throw an exception because the transaction was closed twice.

--- RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -52,6 +52,8 @@ namespace RMDataManager.Library.Internal.DataAccess
             _connection.Open();
 
             _transaction = _connection.BeginTransaction();
+
+            isClosed = false;
         }
 
         public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
@@ -65,29 +67,49 @@ namespace RMDataManager.Library.Internal.DataAccess
         public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
         {
             _connection.Execute(storedProcedure, parameters,
-                commandType: CommandType.StoredProcedure, transaction: _transaction);
+                    commandType: CommandType.StoredProcedure, transaction: _transaction);
         }
+
+        private bool isClosed = false;
 
         public void CommitTransaction()
         {
             _transaction?.Commit();
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void RollbackTransaction()
         {
-            _transaction.Rollback();
+            _transaction?.Rollback();
+            _connection?.Close();
+
+            isClosed = true;
         }
 
         public void Dispose()
         {
-            CommitTransaction();
+            if (isClosed == false)
+            {
+                try
+                {
+                    CommitTransaction();
+                }
+                catch
+                {
+                    // TODO - Log this issue
+                }
+            }
+
+            _transaction = null;
+            _connection = null;
         }
 
-        // Open connection/ start transaction method
-        // Load using the transaction
-        // Save using the transaction
-        // Close connection / stop transaction method
-        // Dispose 
+        // Open connect/start transaction method
+        // load using the transaction
+        // save using the transaction
+        // Close connection/stop transaction method
+        // Dispose
     }
 }
